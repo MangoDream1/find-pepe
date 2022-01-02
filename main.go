934c9af6f513124e2de6ee37e5bd63262f4b78f7
Axel Verheul <axel.verheul@gmail.com>
2022-01-02 15:16:26 +0100

    disallow red boards and webms

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import "go-find-pepe/internal/scraper"
 
 func main() {
-	var allowedHrefSubstrings = []string{"4chan.org", "4channel.org"}
+	var allowedHrefSubstrings = []string{"4channel.org"}
+	// var allowedHrefSubstrings = []string{"4chan.org", "4channel.org"}
 	var requiredHrefSubstrings = []string{"https", "boards."}
-	var allowedImageTypes = []string{".jpg", ".gif", ".png", ".webm"}
+	var allowedImageTypes = []string{".jpg", ".gif", ".png"}
 
 	scraper := scraper.NewScraper(allowedHrefSubstrings, requiredHrefSubstrings, allowedImageTypes)
 	go scraper.ReadDownloadedIds()
 
 	scraper.Start("https://www.4chan.org/")
-
 }
