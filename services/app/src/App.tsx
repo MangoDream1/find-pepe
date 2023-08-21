413bd4271a7a2150331bf5d3458250ec16f74785
Axel <axel.verheul@gmail.com>
2023-08-21 20:18:51 +0200

    cleaned up the frontend

--- services/app/src/App.tsx
+++ services/app/src/App.tsx
@@ -9,7 +9,7 @@ import {
   Select,
 } from "@mui/material";
 import Box from "@mui/material/Box";
-import { useCallback, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 import { OFFSET_SIZE } from "./constants";
 import {
   transformPathToImagePath,
@@ -37,7 +37,6 @@ function App() {
 
   const boards = useGetBoardsByCategory(category);
   const imagePaths = useGetImagePaths({ category, board });
-  const { from, to } = rangeFromOffset(offset);
 
   const onScroll = useCallback(() => {
     const bottom =
@@ -69,81 +68,120 @@ function App() {
 
   return (
     <>
-      <Drawer anchor="top" open={true}>
-        <Paper>
-          <FormControl sx={{ m: 1, minWidth: 80 }}>
-            <InputLabel id="select-category-label">Category</InputLabel>
-            <Select
-              label="Selected category"
-              labelId="select-category-label"
-              id="select-category"
-              onChange={(event) => {
-                if (typeof event.target.value !== "string")
-                  throw new Error("Unexpected value from form");
-                if (event.target.value === "") {
-                  setCategory(undefined);
-                  return;
-                }
-                if (!isCategory(event.target.value)) {
-                  throw new Error("Unexpected category");
-                }
-
-                setCategory(event.target.value);
-              }}
-              value={category || ""}
-            >
-              <MenuItem value={""}>
-                <em>Deselect</em>
-              </MenuItem>
-              <MenuItem value="pepe">Pepe</MenuItem>
-              <MenuItem value="non-pepe">Non-Pepe</MenuItem>
-              <MenuItem value="maybe">Maybe</MenuItem>
-            </Select>
-          </FormControl>
-
-          <FormControl sx={{ m: 1, minWidth: 80 }}>
-            <InputLabel id="select-board-label">Selected board</InputLabel>
-            <Select
-              label="Selected board"
-              labelId="select-board-label"
-              id="select-board"
-              onChange={(event) => {
-                if (typeof event.target.value !== "string")
-                  throw new Error("Unexpected value from form");
-                if (event.target.value === "") {
-                  setBoard(undefined);
-                  return;
-                }
-                setBoard(event.target.value);
-              }}
-              value={board || ""}
-            >
-              <MenuItem value={""}>
-                <em>Deselect</em>
-              </MenuItem>
-              {boards.data.map((b) => (
-                <MenuItem value={b}>{`/${b}/`}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Paper>
-      </Drawer>
-      <Box>
-        <ImageList variant="masonry" cols={5}>
-          {imagePaths.data.slice(from, to).map((s, index) => {
-            return (
-              <ImageListItem key={(index % 5) * 100000000 + index}>
-                <img
-                  src={transformPathToImagePath(s)}
-                  loading="lazy"
-                  style={{ width: "100%" }}
-                />
-              </ImageListItem>
-            );
-          })}
-        </ImageList>
-      </Box>
+      <MenuDrawer>
+        <MenuCategorySelect
+          selectedCategory={category}
+          setCategory={setCategory}
+        />
+        <MenuBoardSelect
+          selectedBoard={board}
+          boards={boards.data}
+          setBoard={setBoard}
+        />
+      </MenuDrawer>
+      <Images imagePaths={imagePaths.data} offset={offset} />
     </>
+  );
+}
+
+function Images(props: { imagePaths: string[]; offset: number }) {
+  const { from, to } = rangeFromOffset(props.offset);
+
+  return (
+    <Box>
+      <ImageList variant="masonry" cols={5}>
+        {props.imagePaths.slice(from, to).map((s, index) => {
+          return (
+            <ImageListItem key={(index % 5) * 100000000 + index}>
+              <img
+                src={transformPathToImagePath(s)}
+                loading="lazy"
+                style={{ width: "100%" }}
+              />
+            </ImageListItem>
+          );
+        })}
+      </ImageList>
+    </Box>
+  );
+}
+
+function MenuDrawer(props: { children: ReactNode[] }) {
+  return (
+    <Drawer anchor="top" open={true}>
+      <Paper>{props.children}</Paper>
+    </Drawer>
+  );
+}
+
+function MenuCategorySelect(props: {
+  selectedCategory: Category | undefined;
+  setCategory: (category: Category | undefined) => void;
+}) {
+  return (
+    <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <InputLabel id="select-category-label">Category</InputLabel>
+      <Select
+        label="Selected category"
+        labelId="select-category-label"
+        id="select-category"
+        onChange={(event) => {
+          if (typeof event.target.value !== "string")
+            throw new Error("Unexpected value from form");
+          if (event.target.value === "") {
+            props.setCategory(undefined);
+            return;
+          }
+          if (!isCategory(event.target.value)) {
+            throw new Error("Unexpected category");
+          }
+
+          props.setCategory(event.target.value);
+        }}
+        value={props.selectedCategory || ""}
+      >
+        <MenuItem value={""}>
+          <em>Deselect</em>
+        </MenuItem>
+        <MenuItem value="pepe">Pepe</MenuItem>
+        <MenuItem value="non-pepe">Non-Pepe</MenuItem>
+        <MenuItem value="maybe">Maybe</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
+function MenuBoardSelect(props: {
+  selectedBoard: string | undefined;
+  boards: string[];
+  setBoard: (board: string | undefined) => void;
+}) {
+  return (
+    <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <InputLabel id="select-board-label">Selected board</InputLabel>
+      <Select
+        label="Selected board"
+        labelId="select-board-label"
+        id="select-board"
+        onChange={(event) => {
+          if (typeof event.target.value !== "string")
+            throw new Error("Unexpected value from form");
+          if (event.target.value === "") {
+            props.setBoard(undefined);
+            return;
+          }
+          props.setBoard(event.target.value);
+        }}
+        value={props.selectedBoard || ""}
+      >
+        <MenuItem value={""}>
+          <em>Deselect</em>
+        </MenuItem>
+        {props.boards.map((b) => (
+          <MenuItem value={b}>{`/${b}/`}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 }
 
